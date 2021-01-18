884b6bebaeb2a43813c17e287e06c86557c758ab
Mish <mishuct26@gmail.com>
2021-01-18 13:04:33 +0200

    Import Switch and Route to switch rendered routes

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,5 +1,6 @@
 import React from "react" 
 import './App.css';
+import {Switch, Route} from "react-router-dom"
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
