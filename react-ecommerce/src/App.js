4353e34d64970048b569c37a57a8c2a6b062ca15
Mish <mishuct26@gmail.com>
2021-01-18 13:07:27 +0200

    Switch Photos and Cart components

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -9,7 +9,14 @@ function App() {
     return (
         <div>
             <Header />
-            <h1>Home Page</h1>
+            <Switch>
+              <Route exact path="/">
+                <Photos />
+              </Route>
+              <Route exact path="/">
+                <Cart />
+              </Route>
+            </Switch>
         </div>
     )
 }
