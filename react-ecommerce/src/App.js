bceef305a18da3b6aaacfd901830d5903bea1e1e
Mish <mishuct26@gmail.com>
2021-01-18 13:10:10 +0200

    Remove exact for Cart

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -13,7 +13,7 @@ function App() {
               <Route exact path="/">
                 <Photos />
               </Route>
-              <Route exact path="/">
+              <Route path="/">
                 <Cart />
               </Route>
             </Switch>
